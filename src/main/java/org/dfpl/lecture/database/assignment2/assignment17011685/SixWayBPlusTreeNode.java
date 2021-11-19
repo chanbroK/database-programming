68e3d79bd6ccf376f510e4fe32c216e11a7064be
BeADia <koc0819@gmail.com>
2021-11-19 16:49:27 +0900

    B+ Tree/add SixWayBPlusTreeNode

--- src/main/java/org/dfpl/lecture/database/assignment2/assignment17011685/SixWayBPlusTreeNode.java
+++ src/main/java/org/dfpl/lecture/database/assignment2/assignment17011685/SixWayBPlusTreeNode.java
@@ -1,13 +1,45 @@
 package org.dfpl.lecture.database.assignment2.assignment17011685;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("unused")
 public class SixWayBPlusTreeNode {
 
-	// Data Abstraction은 예시일 뿐 자유롭게 B+ Tree의 범주 안에서 어느정도 수정가능
-	private SixWayBPlusTreeNode parent;
-	private List<Integer> keyList;
-	private List<SixWayBPlusTreeNode> children;
+    boolean isLeaf;
+    // Data Abstraction은 예시일 뿐 자유롭게 B+ Tree의 범주 안에서 어느정도 수정가능
+    private SixWayBPlusTreeNode parent;
+    private List<Integer> keyList;
+    private List<SixWayBPlusTreeNode> children;
 
+    public SixWayBPlusTreeNode() {
+        keyList = new ArrayList<>();
+        children = new ArrayList<>();
+        isLeaf = true;
+        parent = null;
+    }
+
+    // Getters and Setters
+    public SixWayBPlusTreeNode getParent() {
+        return parent;
+    }
+
+    public void setParent(SixWayBPlusTreeNode parent) {
+        this.parent = parent;
+    }
+
+    public List<Integer> getKeyList() {
+        return keyList;
+    }
+
+    public void setKeyList(List<Integer> keyList) {
+        this.keyList = keyList;
+    }
+
+    public List<SixWayBPlusTreeNode> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<SixWayBPlusTreeNode> children) {
+        this.children = children;
+    }
 }
